949bd41bb2ec82c419a92863969355a1cf12f5fe
W-Mark Kubacki <wmark@hurrikane.de>
2005-12-22 19:52:45 +0000

    Shortened oma_mail.daimon a bit.

--- samples/oma_mail.daimon.php
+++ samples/oma_mail.daimon.php
@@ -11,6 +11,20 @@ if(!function_exists('file_put_contents')) {
 	}
 }
 
+function make_hashfile_of_query($file, $query, $postprocess = true) {
+	global $POSTPROCESS;
+	$fline = '';
+
+	$result = mysql_query($query);
+	while($row = mysql_fetch_row($result)) {
+		$fline .= $row[0]."\t\t".$row[1]."\n";
+	}
+	mysql_free_result($result);
+	file_put_contents($file, $fline);
+	if($postprocess)
+		exec($POSTPROCESS.$file);
+}
+
 $MTA['virtual']	= '/etc/postfix/db/virtual';
 $MTA['regexp']	= '/etc/postfix/db/virtual.regex';
 $MTA['domains']	= '/etc/postfix/db/domains';
@@ -30,15 +44,8 @@ $result = mysql_query('SELECT COUNT(*) AS neue FROM virtual WHERE neu=1');
 	$row = mysql_fetch_assoc($result);
 	mysql_free_result($result);
 if(!is_file($MTA['virtual']) || $row['neue'] > 0 || time()%6<1) {
-	$result = mysql_query('SELECT address,dest FROM virtual WHERE active=1 ORDER BY address DESC');
-	while($row = mysql_fetch_assoc($result)) {
-		$fline .= $row['address']."\t\t".$row['dest']."\n";
-	}
-	mysql_free_result($result);
-	file_put_contents($MTA['virtual'], $fline);
-	unset($fline);
+	make_hashfile_of_query($MTA['virtual'], 'SELECT address,dest FROM virtual WHERE active=1 ORDER BY address DESC');
 	mysql_unbuffered_query('UPDATE LOW_PRIORITY IGNORE virtual SET neu=0 WHERE neu=1');
-	exec($POSTPROCESS.$MTA['virtual']);
 }
 
 // virtual.regexp
@@ -46,26 +53,13 @@ $result = mysql_query('SELECT COUNT(*) AS neue FROM virtual_regexp WHERE neu=1')
 	$row = mysql_fetch_assoc($result);
 	mysql_free_result($result);
 if(!is_file($MTA['regexp']) || $row['neue'] > 0 || time()%6<1) {
-	$result = mysql_query('SELECT reg_exp,dest FROM virtual_regexp WHERE active=1 ORDER BY LENGTH(reg_exp) DESC');
-	while($row = mysql_fetch_assoc($result)) {
-		$fline .= $row['reg_exp']."\t\t".$row['dest']."\n";
-	}
-	mysql_free_result($result);
-	file_put_contents($MTA['regexp'], $fline);
-	unset($fline);
+	make_hashfile_of_query($MTA['regexp'], 'SELECT reg_exp,dest FROM virtual_regexp WHERE active=1 ORDER BY LENGTH(reg_exp) DESC', false);
 	mysql_unbuffered_query('UPDATE LOW_PRIORITY IGNORE virtual_regexp SET neu=0 WHERE neu=1');
 }
 
 // domains
 if(!is_file($MTA['domains']) || time()%48<1) {
-	$result = mysql_query('SELECT domain FROM domains order by (SELECT count(*) FROM virtual WHERE address LIKE CONCAT(\'%\', \'@\', domain)) DESC');
-	while($row = mysql_fetch_assoc($result)) {
-		$fline .= $row['domain']."\t\t".$row['domain']."\n";
-	}
-	mysql_free_result($result);
-	file_put_contents($MTA['domains'], $fline);
-	unset($fline);
-	exec($POSTPROCESS.$MTA['domains']);
+	make_hashfile_of_query($MTA['domains'], 'SELECT domain FROM domains order by (SELECT count(*) FROM virtual WHERE address LIKE CONCAT(\'%\', \'@\', domain)) DESC');
 }
 
 // passwd_cache
