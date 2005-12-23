488174b7012e88b074d04632b1ea0a518f8f8344
W-Mark Kubacki <wmark@hurrikane.de>
2005-12-23 20:09:53 +0000

    Order of appearance in oma_mail.daimon has been changed for easier reading.

--- samples/oma_mail.daimon.php
+++ samples/oma_mail.daimon.php
@@ -1,5 +1,23 @@
 #!/bin/env php
 <?php
+/* *********************** change these according to your environmant ******* */
+$MTA['virtual']	= '/etc/postfix/db/virtual';
+$MTA['regexp']	= '/etc/postfix/db/virtual.regex';
+$MTA['domains']	= '/etc/postfix/db/domains';
+$POSTPROCESS	= '/usr/sbin/postmap %s';
+// Set this to null if you don't use pam_pwdfile for caching.
+$PASSWD_CACHE	= '/var/lib/pam_mysql.cache';
+
+$DB	= array('TYPE'	=> 'mysql',
+		'HOST'	=> 'localhost',
+		'USER'	=> 'yourMySQL-User',
+		'PASS'	=> 'yourMySQL-Passwd',
+		'DB'	=> 'yourMySQL-DB',
+		'PREFIX'=> '',
+		);
+
+/* *********************** functions **************************************** */
+//		Don't edit anything below.
 function make_hashfile_of_query($file, $query, $delimiter = "\t\t", $postprocess = true) {
 	global $POSTPROCESS, $db;
 
@@ -17,20 +35,7 @@ function make_hashfile_of_query($file, $query, $delimiter = "\t\t", $postprocess
 	}
 }
 
-$MTA['virtual']	= '/etc/postfix/db/virtual';
-$MTA['regexp']	= '/etc/postfix/db/virtual.regex';
-$MTA['domains']	= '/etc/postfix/db/domains';
-$PASSWD_CACHE	= '/var/lib/pam_mysql.cache';			// in case you cache your SQL-entries
-$POSTPROCESS	= '/usr/sbin/postmap %s';
-
-$DB	= array('TYPE'	=> 'mysql',
-		'HOST'	=> 'localhost',
-		'USER'	=> 'yourMySQL-User',
-		'PASS'	=> '##MysqlSecret-SELECT-only##',
-		'DB'	=> 'yourMySQL-DB',
-		'PREFIX'=> '',
-		);
-
+/* *********************** logic ******************************************** */
 include('adodb/adodb.inc.php');
 $db	= ADONewConnection($DB['TYPE']);
 $db->Connect($DB['HOST'], $DB['USER'], $DB['PASS'], $DB['DB']) or die('Cannot connect to MySQL Server.');
@@ -55,11 +60,12 @@ if(!is_file($MTA['domains']) || time()%48<1) {
 	make_hashfile_of_query($MTA['domains'], 'SELECT domain FROM '.$DB['PREFIX'].'domains ORDER BY (SELECT count(*) FROM '.$DB['PREFIX'].'virtual WHERE address LIKE CONCAT(\'%\', \'@\', domain)) DESC');
 }
 
-// passwd_cache
+// for pam_pwdfile
 if(!is_null($PASSWD_CACHE) && (!is_file($PASSWD_CACHE) || time()%24<1)) {
 	make_hashfile_of_query($PASSWD_CACHE, 'SELECT mbox, pass_crypt FROM '.$DB['PREFIX'].'user', ':', false);
 }
 
+// optimize DB
 if(time()%50 < 2 && ($DB['TYPE'] == 'mysql' || $DB['TYPE'] == 'mysqli')) {
 	$db->Execute('OPTIMIZE TABLE '.$DB['PREFIX'].'virtual, '.$DB['PREFIX'].'virtual_regexp, '.$DB['PREFIX'].'user, '.$DB['PREFIX'].'domains');
 }
