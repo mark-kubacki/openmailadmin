10482b88a066262ada985bec163db22c1d46baa6
W-Mark Kubacki <wmark@hurrikane.de>
2006-03-05 14:40:20 +0000

    Comments and visibility modifiers.

--- inc/lib/HTMLInputTagGenerator.php
+++ inc/lib/HTMLInputTagGenerator.php
@@ -1,16 +1,17 @@
 <?php
 /**
  * Regard this as huge factory for generating strings of HTML "input" tags.
+ *
+ * @see		http://www.w3.org/TR/html401/interact/forms.html
  */
 class HTMLInputTagGenerator
 {
-	var		$images_dir;			// storage of images (for most derived classes need it)
-	var		$arrClass	= array();	// CSS Class of tag
-	var		$arrProperties	= array();	// tag's properties
+	/** Key of this array contains tag's name and value its CSS class. */
+	public		$arrClass	= array();
+	/** Key of this array contains tag's name and value its tag-properties. */
+	public		$arrProperties	= array();
 
-	// constructor
-	function HTMLInputTagGenerator() {
-		$this->images_dir	= '';
+	public function __construct() {
 		$this->arrClass['text']		= 'text';
 		$this->arrClass['password']	= 'password';
 		$this->arrClass['image']	= 'image';
@@ -26,11 +27,23 @@ class HTMLInputTagGenerator
 		$this->arrProperties['select']	= array();
 	}
 
-	/** For synthesizing generic tags. */
-	function _generic($tag, $arrProperties, $strContent = '', $container = true, $part = 'a') {
+	/**
+	 * For synthesizing generic tags.
+	 * SGML container can enclose text and thus consist of opening and closing sequence. I.e. B of HTML.
+	 *
+	 * @param	tag		Tag's name. I.e. "input".
+	 * @param	arrProperties	Array of properties of the tag.
+	 * @param	strContent	Text enclosed byy the tag.
+	 * @param	container	Boolean. True if tag is SGML container.
+	 * @param	part		"a" for "all parts", "s" for "opening/start sequence" and "e" for "closing/ending sequence"
+	 */
+	private function _generic($tag, $arrProperties, $strContent = '', $container = true, $part = 'a') {
 		$ret = '';
 		if($part != 'e') {
-			$arrProperties	= array_merge(isset($this->arrProperties[$tag]) ? $this->arrProperties[$tag] : $this->arrProperties, $arrProperties);
+			$arrProperties	= array_merge(isset($this->arrProperties[$tag])
+								? $this->arrProperties[$tag]
+								: $this->arrProperties,
+							$arrProperties);
 			$ret	= '<'.$tag;
 			foreach($arrProperties as $key => $value)
 				$ret	.= ' '.strtolower($key).'="'.$value.'"';
@@ -48,7 +61,8 @@ class HTMLInputTagGenerator
 		return $ret;
 	}
 
-	function _generate($type, $name, $value, $arrProperties) {
+	/** For generating input tags. */
+	public function _generate($type, $name, $value, $arrProperties) {
 		$arrProperties['type']	= $type;
 		$arrProperties['name']	= $name;
 		if($type == 'checkbox' || $type == 'radio') {
@@ -69,30 +83,39 @@ class HTMLInputTagGenerator
 		return($this->_generic('input', $arrProperties, '', false));
 	}
 
-	function checkbox($name, $value = null, $prop = array()) {
+	public function checkbox($name, $value = null, $prop = array()) {
 		return($this->_generate('checkbox', $name, $value, $prop));
 	}
 
-	function radio($name, $value, $prop = array()) {
+	public function radio($name, $value, $prop = array()) {
 		return($this->_generate('radio', $name, $value, $prop));
 	}
 
-	function submit($name) {
+	public function submit($name) {
 		return($this->_generate('submit', $name, $name, array()));
 	}
 
-	function text($name, $maxlength = '') {
+	public function text($name, $maxlength = '') {
 		if($maxlength != '' && is_numeric($maxlength))
 			return($this->_generate('text', $name, null, array('maxlength' => $maxlength)));
 		else
 			return($this->_generate('text', $name, null, array()));
 	}
 
-	function textarea($name, $rows = 2, $cols=49) {
-		return($this->_generic('textarea', array('rows' => $rows, 'cols' => $cols, 'name' => $name), isset($_POST[$name]) ? $_POST[$name] : '', true));
+	public function textarea($name, $rows = 2, $cols=49) {
+		return($this->_generic('textarea',
+					array('rows' => $rows, 'cols' => $cols, 'name' => $name),
+					isset($_POST[$name]) ? $_POST[$name] : '', true));
 	}
 
-	function select($name, $arr_names, $arr_values = array(), $size = '1', $multiple = 0) {
+	/**
+	 * @param	name		Name of input field.
+	 * @param	arr_names	Array with names of options which the use can chose from. These will be displayed.
+	 * @param	arr_values	Corresponding values. If none are given an option's value will be equal to its name.
+	 * @param	size		rows visible
+	 * @param	multiple	Set to 1 to allow multiple selection.
+	 */
+	public function select($name, $arr_names, $arr_values = array(), $size = '1', $multiple = 0) {
 		$select_value = '';
 		foreach($arr_names as $key => $value) {
 			$select_value .= '<option';
@@ -109,14 +132,14 @@ class HTMLInputTagGenerator
 		return($this->_generic('select', array('size' => $size, 'name' => $name), $select_value, true));
 	}
 
-	function password($name, $maxlength = '') {
+	public function password($name, $maxlength = '') {
 		if($maxlength != '' && is_numeric($maxlength))
 			return($this->_generate('password', $name, null, array('maxlength' => $maxlength)));
 		else
 			return($this->_generate('password', $name, null, array()));
 	}
 
-	function hidden($name, $value) {
+	public function hidden($name, $value) {
 		return($this->_generate('hidden', $name, $value, array()));
 	}
 
