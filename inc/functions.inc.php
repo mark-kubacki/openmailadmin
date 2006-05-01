9081993a8bc9d1939bfb38c335f95d4d1238f206
W-Mark Kubacki <wmark@hurrikane.de>
2006-05-01 19:31:41 +0000

    {{{array_combine}}} has been removed from source as it is included in PHP 5.

--- inc/functions.inc.php
+++ inc/functions.inc.php
@@ -1,40 +1,4 @@
 <?php
-// from PEAR: PHP_Compat
-if (!function_exists('array_combine')) {
-	function array_combine($keys, $values) {
-		if (!is_array($keys)) {
-			trigger_error('array_combine() expects parameter 1 to be array, ' . gettype($keys) . ' given', E_USER_WARNING);
-			return;
-		}
-
-		if (!is_array($values)) {
-			trigger_error('array_combine() expects parameter 2 to be array, ' . gettype($values) . ' given', E_USER_WARNING);
-			return;
-		}
-
-		if (count($keys) !== count($values)) {
-			trigger_error('array_combine() Both parameters should have equal number of elements', E_USER_WARNING);
-			return false;
-		}
-
-		if (count($keys) === 0 || count($values) === 0) {
-			trigger_error('array_combine() Both parameters should have number of elements at least 0', E_USER_WARNING);
-			return false;
-		}
-
-		$keys    = array_values($keys);
-		$values  = array_values($values);
-
-		$combined = array ();
-
-		for ($i = 0, $cnt = count($values); $i < $cnt; $i++) {
-			$combined[$keys[$i]] = $values[$i];
-		}
-
-		return $combined;
-	}
-}
-
 /*
  * Removes unneccesary whitespace and invokes ob_end_flush().
  */
