f21bc08430f7eaa180b7fede73cbdf9ae76b5c54
W-Mark Kubacki <wmark@hurrikane.de>
2006-08-03 22:53:56 +0000

    Whitespace changes.

--- design/exception.js
+++ design/exception.js
@@ -1,54 +1,57 @@
-    function getElementsByClassName(oElm, strTagName, strClassName){
-        // Written by Jonathan Snook, http://www.snook.ca/jon;
-        // Add-ons by Robert Nyman, http://www.robertnyman.com
-        var arrElements = (strTagName == "*" && document.all)? document.all :
-        oElm.getElementsByTagName(strTagName);
-        var arrReturnElements = new Array();
-        strClassName = strClassName.replace(/\-/g, "\\-");
-        var oRegExp = new RegExp("(^|\\s)" + strClassName + "(\\s|$)");
-        var oElement;
-        for(var i=0; i<arrElements.length; i++){
-            oElement = arrElements[i];
-            if(oRegExp.test(oElement.className)){
-                arrReturnElements.push(oElement);
-            }
-        }
-        return (arrReturnElements)
-    }
-    function hideAll(elems) {
-      for (var e = 0; e < elems.length; e++) {
-        elems[e].style.display = 'none';
-      }
-    }
-    function toggle() {
-      for (var i = 0; i < arguments.length; i++) {
-        var e = document.getElementById(arguments[i]);
-        if (e) {
-          e.style.display = e.style.display == 'none' ? 'block' : 'none';
-        }
-      }
-      return false;
-    }
-    function varToggle(link, id, prefix) {
-      toggle(prefix + id);
-      var s = link.getElementsByTagName('span')[0];
-      var uarr = String.fromCharCode(0x25b6);
-      var darr = String.fromCharCode(0x25bc);
-      s.innerHTML = s.innerHTML == uarr ? darr : uarr;
-      return false;
-    }
-    function sectionToggle(span, section) {
-      toggle(section);
-      var span = document.getElementById(span);
-      var uarr = String.fromCharCode(0x25b6);
-      var darr = String.fromCharCode(0x25bc);
-      span.innerHTML = span.innerHTML == uarr ? darr : uarr;
-      return false;
-    }
+function getElementsByClassName(oElm, strTagName, strClassName) {
+	// Written by Jonathan Snook, http://www.snook.ca/jon;
+	// Add-ons by Robert Nyman, http://www.robertnyman.com
+	var arrElements = (strTagName == "*" && document.all)? document.all : oElm.getElementsByTagName(strTagName);
+	var arrReturnElements = new Array();
+	strClassName = strClassName.replace(/\-/g, "\\-");
+	var oRegExp = new RegExp("(^|\\s)" + strClassName + "(\\s|$)");
+	var oElement;
+	for(var i=0; i<arrElements.length; i++) {
+		oElement = arrElements[i];
+		if(oRegExp.test(oElement.className)) {
+			arrReturnElements.push(oElement);
+		}
+	}
+	return (arrReturnElements)
+}
 
-    window.onload = function() {
-      hideAll(getElementsByClassName(document, 'table', 'vars'));
-      hideAll(getElementsByClassName(document, 'div', 'context'));
-      hideAll(getElementsByClassName(document, 'ul', 'traceback'));
-      hideAll(getElementsByClassName(document, 'div', 'section'));
-    }
+function hideAll(elems) {
+	for (var e = 0; e < elems.length; e++) {
+		elems[e].style.display = 'none';
+	}
+}
+
+function toggle() {
+	for (var i = 0; i < arguments.length; i++) {
+		var e = document.getElementById(arguments[i]);
+		if (e) {
+			e.style.display = e.style.display == 'none' ? 'block' : 'none';
+		}
+	}
+	return false;
+}
+
+function varToggle(link, id, prefix) {
+	toggle(prefix + id);
+	var s = link.getElementsByTagName('span')[0];
+	var uarr = String.fromCharCode(0x25b6);
+	var darr = String.fromCharCode(0x25bc);
+	s.innerHTML = s.innerHTML == uarr ? darr : uarr;
+	return false;
+}
+
+function sectionToggle(span, section) {
+	toggle(section);
+	var tspan = document.getElementById(span);
+	var uarr = String.fromCharCode(0x25b6);
+	var darr = String.fromCharCode(0x25bc);
+	tspan.innerHTML = tspan.innerHTML == uarr ? darr : uarr;
+	return false;
+}
+
+window.onload = function() {
+	hideAll(getElementsByClassName(document, 'table', 'vars'));
+	hideAll(getElementsByClassName(document, 'div', 'context'));
+	hideAll(getElementsByClassName(document, 'ul', 'traceback'));
+	hideAll(getElementsByClassName(document, 'div', 'section'));
+}
--- inc/exception_handler.php
+++ inc/exception_handler.php
@@ -6,22 +6,21 @@
 set_exception_handler('PrettyBlueScreen');
 
 function PrettyBlueScreen($e) {
-  $o = create_function('$in','echo htmlspecialchars($in);');
-  $sub = create_function('$f','$loc="";if(isset($f["class"])){
-    $loc.=$f["class"].$f["type"];}
-    if(isset($f["function"])){$loc.=$f["function"];}
-    if(!empty($loc)){$loc=htmlspecialchars($loc);
-    $loc="<strong>$loc</strong>";}return $loc;');
-  $parms = create_function('$f','$params=array();if(isset($f["function"])){
-    try{if(isset($f["class"])){
-    $r=new ReflectionMethod($f["class"]."::".$f["function"]);}
-    else{$r=new ReflectionFunction($f["function"]);}
-    return $r->getParameters();}catch(Exception $e){}}
-    return $params;');
-  $src2lines = create_function('$file','$src=nl2br(highlight_file($file,TRUE));
-    return explode("<br />",$src);');
-  $clean = create_function('$line','return trim(strip_tags($line));');
-  $desc = get_class($e)." making ".$_SERVER['REQUEST_METHOD']." request to ".
-    $_SERVER['REQUEST_URI'];
-  include('./templates/ExceptionHandler.tpl');
+	$o = create_function('$in', 'echo htmlspecialchars($in);');
+	$sub = create_function('$f', '$loc="";if(isset($f["class"])){
+		$loc.=$f["class"].$f["type"];}
+		if(isset($f["function"])){$loc.=$f["function"];}
+		if(!empty($loc)){$loc=htmlspecialchars($loc);
+		$loc="<strong>$loc</strong>";}return $loc;');
+	$parms = create_function('$f', '$params=array();if(isset($f["function"])){
+		try{if(isset($f["class"])){
+		$r=new ReflectionMethod($f["class"]."::".$f["function"]);}
+		else{$r=new ReflectionFunction($f["function"]);}
+		return $r->getParameters();}catch(Exception $e){}}
+		return $params;');
+	$src2lines = create_function('$file', '$src=nl2br(highlight_file($file,TRUE));
+		return explode("<br />",$src);');
+	$clean = create_function('$line', 'return trim(strip_tags($line));');
+	$desc = get_class($e)." making ".$_SERVER['REQUEST_METHOD']." request to ".$_SERVER['REQUEST_URI'];
+	include('./templates/ExceptionHandler.tpl');
 }
