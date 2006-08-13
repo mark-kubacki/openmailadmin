59b303cdba87b62e48fe748eaef2686b9be695bb
W-Mark Kubacki <wmark@hurrikane.de>
2006-08-13 12:19:47 +0000

    ''file_uploads'' doesn't apply for Openmailadmin. Removed.

--- setup.php
+++ setup.php
@@ -153,7 +153,6 @@ switch($_GET['step']) {
 	case '1':
 		$expectations
 		= array('asp_tags'			=> 0,
-			'file_uploads'			=> 0,
 			'display_errors'		=> 0,
 			'log_errors'			=> 1,
 			'ignore_repeated_errors'	=> 1,
