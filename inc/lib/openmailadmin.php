939f4fd1bb55954be1428533ad3f79636e13e805
W-Mark Kubacki <wmark@hurrikane.de>
2007-05-30 12:03:14 +0000

    allow domain names like 163.com (which are numeric only except for TLD)

--- inc/lib/openmailadmin.php
+++ inc/lib/openmailadmin.php
@@ -20,8 +20,8 @@ class openmailadmin
 
 	// "alias" == "local part"
 	const	regex_valid_alias	= '(?=^.{1,64}$)[a-z0-9!$*+\-_]+(?:\.[a-z0-9!$*+\-_]+)*';
-	const	regex_valid_email	= '[a-z0-9!$*+\-_]+(?:\.[a-z0-9!$*+\-_]+)*@(?:(?:(?!\d+\.|-)[a-z0-9_\-]{1,63}(?<!-)\.?)+(?:(?<!\.)\.[a-z]{2,}))';
-	const	regex_valid_domain	= '(?=^.{1,254}$)(?:^localhost$)|(?:^(?:(?!\d+\.|-)[a-z0-9_\-]{1,63}(?<!-)\.?)+(?:(?<!\.)\.[a-z]{2,})$)';
+	const	regex_valid_email	= '[a-z0-9!$*+\-_]+(?:\.[a-z0-9!$*+\-_]+)*@(?:(?:(?![.-])[a-z0-9_\-]{1,63}(?<!-)\.?)+(?:(?<!\.)\.[a-z]{2,}))';
+	const	regex_valid_domain	= '(?=^.{1,254}$)(?:^localhost$)|(?:^(?:(?![.-])[a-z0-9_\-]{1,63}(?<!-)\.?)+(?:(?<!\.)\.[a-z]{2,})$)';
 
 	function __construct(ADOConnection $adodb_handler, array $tablenames, array $cfg, IMAP_Administrator $imap) {
 		$this->db		= $adodb_handler;
